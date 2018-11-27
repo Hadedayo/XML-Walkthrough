8cfe5ae4b0e6b80a9cc25d9ce964ee6f16e180d6
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-27 11:11:18 +0000

    Created method to get all prices in the menu

--- XMLLab/lib/Guiseppes.rb
+++ XMLLab/lib/Guiseppes.rb
@@ -8,6 +8,14 @@ class GuiseppesMenu
     @menu = Nokogiri::XML(File.open('./guiseppes_menu.xml'))
   end
 
-  # Place your methods here
+  # Gets all prices as floats
+  def get_all_prices_as_floats
+    price_array = []
+
+    @menu.xpath('//price').each do |price|
+      price_array << price.text.gsub('£', '').to_f
+    end
+    price_array
+  end
 
 end
