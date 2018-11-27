e2f506dacfeb75f38ceb465f2ad3facc98c79498
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-27 11:31:11 +0000

    Testing responds that prices should be > 10. Testing responds that there are no items with calories over 1000 except for the full breakfast. Testing to show that with all waffle dishes stating you get two waffles

--- XMLLab/spec/xml_menu_spec.rb
+++ XMLLab/spec/xml_menu_spec.rb
@@ -1,23 +1,34 @@
-require 'spec_helper'
-
 describe "XML menu" do
 
   before(:all) do
     @xml_menu = GuiseppesMenu.new
   end
 
-  it "no price should be more than £10" do
-    pending
+  it "should have no prices should be more than £10" do
+    #loop through the prices and do assertion to check its not > 10
+    @xml_menu.get_all_prices_as_floats.each do |price|
+      expect(price).to be <= 10
+    end
   end
 
   it "should have no item with calories over 1000 except for the full breakfast" do
-    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
-    pending
+    @xml_menu.get_all_food_items.each do |food|
+      if food.element_children[0].text == 'Full Breakfast'
+        expect(food.element_children[3].text.to_i).to be > 1000
+      else
+        expect(food.element_children[3].text.to_i).to be < 1000
+      end
+    end
   end
 
   it "should have all waffle dishes stating you get two waffles" do
-    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
-    pending
+  @xml_menu.get_all_food_items.each do |food|
+    if food.element_children[0].text.include? 'waffles'
+      expect(food.element_children[2].text).to include 'Two'
+    end
   end
+  end
+
+
 
 end
